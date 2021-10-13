d572cd9bc7f0ff746adaeec3da89deb16c77707b
jingxiaolin <jingxiaolin@qiyi.com>
2021-10-13 18:33:23 +0800

    feat(提交二叉树代码):

--- leecodetest/src/main/java/tree/TreeNodeLeecode.java
+++ leecodetest/src/main/java/tree/TreeNodeLeecode.java
@@ -3,15 +3,17 @@ package tree;
 public class TreeNodeLeecode {
     public static void main(String[] args) {
         TreeNode rootNode = new TreeNode(1);
-        //rootNode.setLeftNode(new TreeNode(2));
+        rootNode.setLeftNode(new TreeNode(2));
         rootNode.setRightNode(new TreeNode(3));
-        //rootNode.getLeftNode().setLeftNode(new TreeNode(4));
-        rootNode.getRightNode().setRightNode(new TreeNode(5));
+        rootNode.getLeftNode().setLeftNode(new TreeNode(4));
+        /*rootNode.getRightNode().setRightNode(new TreeNode(5));
         rootNode.getRightNode().getRightNode().setRightNode(new TreeNode(6));
-        rootNode.getRightNode().getRightNode().getRightNode().setLeftNode(new TreeNode(7));
-        System.out.println("二叉树的最大深度 == "+treeNodeMininumDeep(rootNode));
+        rootNode.getRightNode().getRightNode().getRightNode().setLeftNode(new TreeNode(7));*/
+        //System.out.println("完全二叉树节点数 == "+treeNodeCount(rootNode));
+        System.out.println("完全二叉树节点数(new) == "+treeNodeCountNew(rootNode));
     }
     //1、二叉树的最大深度
+    //leecode：er-cha-shu-de-shen-du-lcof
     private static int treeNodeDeep(TreeNode rootNode){
         if(rootNode == null){
             return 0;
@@ -21,7 +23,8 @@ public class TreeNodeLeecode {
         return leftDeep>rightDeep?leftDeep+1:rightDeep+1;
     }
 
-    //1、二叉树的最小深度
+    //2、二叉树的最小深度
+    //leecode:minimum-depth-of-binary-tree
     //[2,null,3,null,4,null,5,null,6]
     private static int treeNodeMininumDeep(TreeNode rootNode){
         if(rootNode == null){
@@ -29,9 +32,55 @@ public class TreeNodeLeecode {
         }
         int leftDeep = treeNodeDeep(rootNode.getLeftNode());
         int rightDeep = treeNodeDeep(rootNode.getRightNode());
+        //加这个判断的目的是，leecode 题目要求，求的是根节点到叶子节点的距离，如果没有叶子节点，
+        //那么计算的深度无效
         if(leftDeep == 0 || rightDeep == 0){
             return leftDeep + rightDeep +1;
         }
         return leftDeep<rightDeep?leftDeep+1:rightDeep+1;
     }
+
+    //3、完全二叉树节点个数
+    //leecode:count-complete-tree-nodes
+    //暴力法，不可取，没有利用完全二叉树的特性。
+    private static int treeNodeCount(TreeNode rootNode){
+        if(rootNode == null){
+            return 0;
+        }
+        int leftDeep = treeNodeCount(rootNode.getLeftNode());
+        int rightDeep = treeNodeCount(rootNode.getRightNode());
+        return leftDeep+rightDeep+1;
+    }
+
+    //完全二叉树的定义如下：
+    // 在完全二叉树中，除了最底层节点可能没填满外，其余每层节点数都达到最大值，
+    // 并且最下面一层的节点都集中在该层最左边的若干位置。
+    // 若最底层为第 h 层，则该层包含 1~ 2^h 个节点。
+    private static int treeNodeCountNew(TreeNode rootNode){
+        if(rootNode == null){
+            return 0;
+        }
+        int leftDeep = treeNodeDeepForCountNew(rootNode.getLeftNode(),"left");
+        int rightDeep = treeNodeDeepForCountNew(rootNode.getRightNode(),"right");
+        if(leftDeep == rightDeep){
+            return (int) (Math.pow(2,leftDeep)-1);
+        }else {
+            return treeNodeCountNew(rootNode.getLeftNode()) + treeNodeCountNew(rootNode.getRightNode())+1;
+        }
+    }
+
+    private static int treeNodeDeepForCountNew(TreeNode rootNode,String lr){
+        int deepRes = 1;
+        while (null != rootNode){
+            deepRes++;
+            if(lr.equals("left")){
+                rootNode = rootNode.getLeftNode();
+            }else {
+                rootNode = rootNode.getRightNode();
+            }
+        }
+        return deepRes;
+    }
+
+    //4、
 }
